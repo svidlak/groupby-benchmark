121daed87ec14bccc6a60f0c7c13b70c249301f3
svidlak <svid320@gmail.com>
2023-12-16 21:42:11 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:21
+WORKDIR /app
+COPY . .
+CMD npm start
---
+++ index.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const filePath = `${__dirname}/1k_birthdates.json`;
+
+const peoplesList = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+const groupByForLoop = () => {
+    console.time('groupByForLoop');
+    const peopleGroupedByYearOfBirth = {};
+
+    for (let i = 0; i < peoplesList.length; i++) {
+        const person = peoplesList[i];
+        const { birth_year } = person;
+
+        if (!peopleGroupedByYearOfBirth[birth_year]) {
+            peopleGroupedByYearOfBirth[birth_year] = [];
+        }
+
+        peopleGroupedByYearOfBirth[birth_year].push(person);
+    }
+
+    console.timeEnd('groupByForLoop');
+    logGroupsLength(peopleGroupedByYearOfBirth)
+}
+
+const groupByReduce = () => {
+    console.time('groupByReduce');
+
+    const peopleGroupedByYearOfBirth = peoplesList.reduce((map, person) => {
+        const { birth_year } = person;
+
+        if (!map[birth_year]) {
+            map[birth_year] = [];
+        }
+
+        map[birth_year].push(person);
+        return map;
+    }, {});
+
+    console.timeEnd('groupByReduce');
+    logGroupsLength(peopleGroupedByYearOfBirth)
+}
+
+const groupByForOf = () => {
+    console.time('groupByForOf');
+
+    const peopleGroupedByYearOfBirth = {};
+
+    for (let person of peoplesList) {
+        const { birth_year } = person;
+
+        if (!peopleGroupedByYearOfBirth[birth_year]) {
+            peopleGroupedByYearOfBirth[birth_year] = [];
+        }
+
+        peopleGroupedByYearOfBirth[birth_year].push(person);
+    }
+
+    console.timeEnd('groupByForOf');
+    logGroupsLength(peopleGroupedByYearOfBirth);
+}
+
+const forEachGroupBy = () => {
+    console.time('forEachGroupBy');
+
+    const map = {};
+    peoplesList.forEach(person => {
+        const { birth_year } = person;
+
+        if (!map[birth_year]) {
+            map[birth_year] = [];
+        }
+
+        map[birth_year].push(person);
+    })
+
+    console.timeEnd('forEachGroupBy');
+    logGroupsLength(map)
+}
+
+const objectGroupBy = () => {
+    console.time('objectGroupBy');
+
+    const peopleGroupedByYearOfBirth = Object.groupBy(peoplesList, ({ birth_year }) => birth_year);
+
+    console.timeEnd('objectGroupBy');
+    logGroupsLength(peopleGroupedByYearOfBirth)
+}
+
+const logGroupsLength = (mapObject) => {
+    return
+    Object.keys(mapObject).forEach(key => {
+        console.log(`${key}: ${mapObject[key].length}`);
+    })
+}
+
+// groupByForLoop();
+// groupByReduce();
+// groupByForOf();
+// objectGroupBy();
+forEachGroupBy();
